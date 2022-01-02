15debbbd65f344f0757f08633d2ed0bcc17ef0ed
eagles70 <jkkim70@gmail.com>
2022-01-02 23:58:29 +0900

    text padding

--- components/youtube.jsx
+++ components/youtube.jsx
@@ -8,7 +8,7 @@ const Youtube = ({youtubeArr}) => {
 
   return (  
     <div className={styles.container}>
-      <div className={styles.title}>인기 Youtube 동영상</div>      
+      <div className={styles.title}>인기 Youtube</div>      
       { videoId && <Video videoId={videoId} /> }
       {
         youtubeArr.map((a, i)=>{
