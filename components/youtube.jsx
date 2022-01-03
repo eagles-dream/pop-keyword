3f43cb729a5e66df2c86eb18b5444c3d9526c4c0
eagles70 <jkkim70@gmail.com>
2022-01-03 16:09:57 +0900

    useRef

--- components/youtube.jsx
+++ components/youtube.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Video from './video';
 import styles from './youtube.module.css'
 
 const Youtube = ({youtubeArr}) => {
   //console.log(youtubeArr)
   const [videoId, setVideoId] = useState(null)
+  const playerRef = useRef()
+  const goToPlayer = () => {
+    playerRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
 
   return (  
     <div className={styles.container}>
       <div className={styles.title}>인기 Youtube</div>      
-      { videoId && <Video videoId={videoId} /> }
+      <div ref={playerRef}>{ videoId && <Video videoId={videoId} /> }</div>
       {
         youtubeArr.map((a, i)=>{
           return (
-            <div className={styles.card} key={i} onClick={()=>{setVideoId(a.id)}}>
+            <div className={styles.card} 
+                 key={i} 
+                 onClick={()=>{
+                   setVideoId(a.id);
+                   goToPlayer();
+                 }}>
               <img className={styles.img} alt="youtube" src={a.snippet.thumbnails.default.url}></img>
               <div className={styles.order}>
                 <span className={styles.number}>인기 {i+1}위</span>
