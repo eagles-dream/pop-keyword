85075ba9cdc352340bc5cfb27ecd5ff452d0c001
eagles70 <jkkim70@gmail.com>
2022-01-07 00:19:41 +0900

    rel="noreferrer"

--- components/banner/bannermobile.jsx
+++ components/banner/bannermobile.jsx
@@ -3,7 +3,7 @@ import styles from './bannermobile.module.css'
 const BannerMobile = () => {
   return ( 
     <div className={styles.container}>
-        <a className={styles.center} href="https://coupa.ng/cbrPAP" target="_blank" referrerPolicy="unsafe-url">
+        <a className={styles.center} href="https://coupa.ng/cbrPAP" target="_blank" referrerPolicy="unsafe-url" rel="noreferrer">
           <img src="https://ads-partners.coupang.com/banners/26883?subId=&traceId=V0-301-879dd1202e5c73b2-I26883&w=320&h=50" alt=""/>
         </a>
     </div>
--- components/banner/bannerpc.jsx
+++ components/banner/bannerpc.jsx
@@ -3,10 +3,10 @@ import styles from './bannerpc.module.css'
 const BannerPc = () => {
   return ( 
     <div className={styles.container}>
-        <a className={styles.left} href="https://coupa.ng/cbrOjO" target="_blank" referrerPolicy="unsafe-url">
+        <a className={styles.left} href="https://coupa.ng/cbrOjO" target="_blank" referrerPolicy="unsafe-url" rel="noreferrer">
           <img src="https://ads-partners.coupang.com/banners/545320?subId=&traceId=V0-301-969b06e95b87326d-I545320&w=160&h=600" alt=""/>
         </a>
-        <a className={styles.right} href="https://coupa.ng/cbrNMT" target="_blank" referrerPolicy="unsafe-url">
+        <a className={styles.right} href="https://coupa.ng/cbrNMT" target="_blank" referrerPolicy="unsafe-url" rel="noreferrer">
           <img src="https://ads-partners.coupang.com/banners/165511?subId=&traceId=V0-301-371ae01f4226dec2-I165511&w=160&h=600" alt=""/>
         </a>
     </div>
