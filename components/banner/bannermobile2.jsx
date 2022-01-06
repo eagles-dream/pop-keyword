d75d3112d949fc95491b61444fbe3b7bbae2963e
eagles70 <jkkim70@gmail.com>
2022-01-07 00:50:16 +0900

    position

--- components/banner/bannermobile2.jsx
+++ components/banner/bannermobile2.jsx
@@ -3,7 +3,7 @@ import styles from './bannermobile2.module.css'
 const BannerMobile2 = () => {
   return ( 
     <div className={styles.container}>
-        <a className={styles.center} href="https://coupa.ng/cbrSb1" target="_blank" referrerpolicy="unsafe-url" rel="noreferrer">
+        <a className={styles.center} href="https://coupa.ng/cbrSb1" target="_blank" referrerPolicy="unsafe-url" rel="noreferrer">
           <img src="https://ads-partners.coupang.com/banners/546377?subId=&traceId=V0-301-50c6c2b97fba9aee-I546377&w=320&h=50" alt=""/>
         </a>
     </div>
