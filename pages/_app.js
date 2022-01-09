d01fe8368f901eaf8cdde1abd1210a7acd1f1e27
eagles70 <jkkim70@gmail.com>
2022-01-09 14:57:13 +0900

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
@@ -16,3 +33,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
