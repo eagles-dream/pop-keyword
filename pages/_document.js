73b0ba9a07ffefcb668e0107c9cec451ae8c0b8d
eagles70 <jkkim70@gmail.com>
2022-01-10 18:05:06 +0900

    구글 애드센스

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,9 @@ class MyDocument extends Document {
     return (
       <Html lang='ko'>
         <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4648443025311516"
+            crossOrigin="anonymous"></script>
+
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
           <script
             dangerouslySetInnerHTML={{
