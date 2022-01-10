e579f43a483afe14e11770597dbe294e9e353324
eagles70 <jkkim70@gmail.com>
2022-01-10 16:15:39 +0900

    푸터

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,8 @@ import styles from './footer.module.css'
 const Footer = () => {
   return(
     <div className={styles.container}>
-      <span className={styles.copyright}>© Copyright Search</span>
+      <span className={styles.copyright}>실시간 인기검색어 서비스 랭크보드</span>
+      <span className={styles.copyright}>© Copyright RankBoard</span>
     </div>
   )
 }
