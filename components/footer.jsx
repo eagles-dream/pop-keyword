2befebb5381c74b9dd6631a3ff3fba772ee6e2b2
eagles70 <jkkim70@gmail.com>
2022-01-10 16:22:17 +0900

    contack email

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return(
     <div className={styles.container}>
       <span className={styles.copyright}>실시간 인기검색어 서비스 랭크보드</span>
+      <span className={styles.copyright}>Contack Email : everyrank@gmail.com</span>
       <span className={styles.copyright}>© Copyright RankBoard</span>
     </div>
   )
