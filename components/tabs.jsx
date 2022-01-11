ef7f1a4d51c81bacc019677be428a758031716fa
eagles70 <jkkim70@gmail.com>
2022-01-11 16:42:48 +0900

    tab, border

--- components/tabs.jsx
+++ components/tabs.jsx
@@ -6,18 +6,18 @@ const Tabs = ({tab1, tab2, tab3, tab4, Tab1, Tab2, Tab3, Tab4}) => {
     <div className={styles.container}>
       {
         tab1
-        ? <span className={styles.title_active} onClick={Tab1}>국내기준</span>
-        : <span className={styles.title} onClick={Tab1}>국내기준</span>
+        ? <span className={styles.title_active} onClick={Tab1}>국내</span>
+        : <span className={styles.title} onClick={Tab1}>국내</span>
       }
       {
         tab2
-        ? <span className={styles.title_active} onClick={Tab2}>구글기준</span>
-        : <span className={styles.title} onClick={Tab2}>구글기준</span>
+        ? <span className={styles.title_active} onClick={Tab2}>구글</span>
+        : <span className={styles.title} onClick={Tab2}>구글</span>
       }      
       {
         tab3
-        ? <span className={styles.title_active} onClick={Tab3}>쿠팡기준</span>
-        : <span className={styles.title} onClick={Tab3}>쿠팡기준</span>
+        ? <span className={styles.title_active} onClick={Tab3}>쿠팡</span>
+        : <span className={styles.title} onClick={Tab3}>쿠팡</span>
       }      
       {
         tab4
